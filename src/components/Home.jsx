8c2c6a82bd7215b1983018d73c368286ae6c09b4
Fabrizio Pio Caponio <fabrizio.caponio99@gmail.com>
2023-07-10 16:29:43 +0200

    aggiusti

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const HomeCopy = "Realizzo Web Applications e siti web funzionali, prestando una
                     </Link>
                 </div>
                 <div className='flex flex-col justify-center h-ful group'>
-                    <img className=' w-3/5 md:w-2/3 z-10 mx-auto md:ml-48 pt-20 sm:w-2/5 group-hover:scale-125 duration-700' src={HeroImage} alt="Immagine Profilo" />
+                    <img className=' w-3/5 md:w-2/3 z-10 mx-auto md:ml-48 md:pt-20 sm:w-2/5 group-hover:scale-125 duration-700' src={HeroImage} alt="Immagine Profilo" />
                     <svg className='group-hover:rotate-180 group-hover:scale-75 duration-1000 absolute z-0 max-w-2xl ' viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                         <path fill="#E9E41D" d="M55.3,-65.8C71.1,-52.7,82.9,-34.7,86.2,-15.3C89.5,4.1,84.3,24.9,72.7,39C61,53.1,42.9,60.5,24.4,67.4C5.9,74.3,-12.9,80.7,-30.9,77.4C-49,74.1,-66.3,61.2,-73,44.6C-79.7,27.9,-75.8,7.6,-70.3,-10.4C-64.8,-28.5,-57.6,-44.2,-45.6,-58C-33.6,-71.8,-16.8,-83.5,1.5,-85.3C19.8,-87.1,39.6,-78.9,55.3,-65.8Z" transform="translate(100 100)" />
                     </svg>
